329f0e4d05fe293149fede8d1ba4fa080ec77c5b
soblab <soblab@Whit216-07.it.farmingdale.edu>
2022-02-02 10:37:11 -0500

    InputOutput from file

--- src/main/java/com/mycompany/testfileio/NewClass.java
+++ src/main/java/com/mycompany/testfileio/NewClass.java
@@ -1,17 +1,27 @@
 
 package com.mycompany.testfileio;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Scanner;
 
 
 public class NewClass {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         System.out.println("abc");
-        Scanner keyboard = new Scanner(System.in);
+//        Scanner keyboard = new Scanner(System.in);
+        FileReader fr = new FileReader("Input.txt");
+        Scanner inFile = new Scanner(fr);
+        while(inFile.hasNext()){
+        String f = inFile.nextLine();
+        System.out.println("The file's next line is: ");
+        System.out.println(f);
+        }
         
-        String s = keyboard.nextLine();
         
-        System.out.println("The user typed in : ");
-        System.out.println(s);
+        
+//        String s = keyboard.nextLine();
+//        System.out.println("The user typed in : ");
+//        System.out.println(s);
     }
 }
